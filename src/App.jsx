1d51be9caa5361e4e1714012687696bd6a8fb8be
Cauê Moreira <162379957+Caue06@users.noreply.github.com>
2024-10-17 21:25:16 -0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import RelogioDigital from './components/RelogioDigital'
+
+function App() {
+
+  return (
+    <>
+    <RelogioDigital/>
+    </>
+  )
+}
+
+export default App
